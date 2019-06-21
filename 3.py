166bd615dda77b726511f237da531b6a3667668f
srimathiselvakumaran <51949530+srimathiselvakumaran@users.noreply.github.com>
2019-06-21 12:19:48 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+n=str(input())
+if(n=int(input())):
+    print("invalid")
+if(n=="a"orn=="A"orn=="E"orn=="e"n=="i"orn=="I"orn=="o"orn=="O"n=="u"orn=="U"):
+    print("Vowel")
+else:
+    print("Consonant")
