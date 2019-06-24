c5ce5953496e412941d7bb9e21babace7b3da768
srimathiselvakumaran <51949530+srimathiselvakumaran@users.noreply.github.com>
2019-06-24 15:40:45 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,8 @@
-n=str(input())
-if(n=int(input())):
-    print("invalid")
-if(n=="a"orn=="A"orn=="E"orn=="e"n=="i"orn=="I"orn=="o"orn=="O"n=="u"orn=="U"):
+n=input()
+if(n=="a"or n=="A"or n=="E"or n=="e"or n=="i"or n=="I"or n=="o"or n=="O"or n=="u"or n=="U"):
     print("Vowel")
-else:
+elif(n=="b" or n=="B"or  n=="c" or n=="C" or n=="d" or n=="D"or n=="f" or n=="F"or n=="g" or n=="G"or n=="h" or n=="H"or n=="J" or n=="j"or n=="K" or n=="k"or n=="l" or n=="L"or n=="m" or n=="M"or n=="n" or n=="N"or n=="p" or n=="P"or n=="q" or n=="Q"or n=="r" or n=="R"or n=="s" or n=="S"or n=="t" or n=="T"or n=="v" or n=="V"or n=="w" or n=="W" or n=="x" or n=="X" or  n=="y" or n=="Y" or n=="z" or n=="Z"):
     print("Consonant")
+else:
+        print("invalid")
+
