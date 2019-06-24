0ec24dc8039e0f4bca49edc8ec1e5059f0d197fe
srimathiselvakumaran <51949530+srimathiselvakumaran@users.noreply.github.com>
2019-06-24 15:46:35 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,5 +4,5 @@ if(n=="a"or n=="A"or n=="E"or n=="e"or n=="i"or n=="I"or n=="o"or n=="O"or n=="u
 elif(n=="b" or n=="B"or  n=="c" or n=="C" or n=="d" or n=="D"or n=="f" or n=="F"or n=="g" or n=="G"or n=="h" or n=="H"or n=="J" or n=="j"or n=="K" or n=="k"or n=="l" or n=="L"or n=="m" or n=="M"or n=="n" or n=="N"or n=="p" or n=="P"or n=="q" or n=="Q"or n=="r" or n=="R"or n=="s" or n=="S"or n=="t" or n=="T"or n=="v" or n=="V"or n=="w" or n=="W" or n=="x" or n=="X" or  n=="y" or n=="Y" or n=="z" or n=="Z"):
     print("Consonant")
 else:
-        print("invalid")
+    print("invalid")
 
