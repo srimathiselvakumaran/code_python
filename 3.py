6938b892b339e74dc4003380599387dbca83f949
srimathiselvakumaran <51949530+srimathiselvakumaran@users.noreply.github.com>
2019-06-25 14:24:59 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,8 +1,8 @@
-a=input()
-if(a=='a'or a=='e' or a=='i' or a=='o'or a=='u' or a=='A'or a=='E' or a=='I' or a=='O'or a=='U'):
-    print("Vowel")
-elif(a=='b' or a=='c' or a=='d'or a=='f'or a=='g'or a=='h'or a=='j'or a=='k' or a=='l' or a=='m' or a=='n' or a=='p' or a=='q' or a=='r' or a=='s' or a=='t' or a=='v' or a=='w' or a=='x' or a=='y' or a=='z' or a=='A' or a=='B' or a=='C' or a=='D'or a=='F'or a=='G'or a=='H'or a=='J'or a=='K' or a=='L' or a=='M' or a=='N' or a=='P' or a=='Q' or a=='R' or a=='S' or a=='T' or a=='V' or a=='W' or a=='X' or a=='Y' or a=='Z'):
-    print("Consonant")
+n=input()
+if a.isalpha():
+    if n=='a'or n=='A'or n=='e'or n=='E'or n=='i'or n=='I'or n=='o'or n=='O'or n=='u'or n=='U':
+        print("Vowel")
+    else:
+        print("Consonant")
 else:
     print("Invalid")
-
