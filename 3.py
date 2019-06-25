ca75a142af3440693a23b4bf1a1a8fe9dc441c8c
srimathiselvakumaran <51949530+srimathiselvakumaran@users.noreply.github.com>
2019-06-25 14:21:06 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,8 +1,8 @@
-n=input()
-if(n=="a"or n=="A"or n=="E"or n=="e"or n=="i"or n=="I"or n=="o"or n=="O"or n=="u"or n=="U"):
+a=input()
+if(a=='a'or a=='e' or a=='i' or a=='o'or a=='u' or a=='A'or a=='E' or a=='I' or a=='O'or a=='U'):
     print("Vowel")
-elif(n=="b" or n=="B"or  n=="c" or n=="C" or n=="d" or n=="D"or n=="f" or n=="F"or n=="g" or n=="G"or n=="h" or n=="H"or n=="J" or n=="j"or n=="K" or n=="k"or n=="l" or n=="L"or n=="m" or n=="M"or n=="n" or n=="N"or n=="p" or n=="P"or n=="q" or n=="Q"or n=="r" or n=="R"or n=="s" or n=="S"or n=="t" or n=="T"or n=="v" or n=="V"or n=="w" or n=="W" or n=="x" or n=="X" or  n=="y" or n=="Y" or n=="z" or n=="Z"):
+elif(a=='b' or a=='c' or a=='d'or a=='f'or a=='g'or a=='h'or a=='j'or a=='k' or a=='l' or a=='m' or a=='n' or a=='p' or a=='q' or a=='r' or a=='s' or a=='t' or a=='v' or a=='w' or a=='x' or a=='y' or a=='z' or a=='A' or a=='B' or a=='C' or a=='D'or a=='F'or a=='G'or a=='H'or a=='J'or a=='K' or a=='L' or a=='M' or a=='N' or a=='P' or a=='Q' or a=='R' or a=='S' or a=='T' or a=='V' or a=='W' or a=='X' or a=='Y' or a=='Z'):
     print("Consonant")
 else:
-    print("invalid")
+    print("Invalid")
 
